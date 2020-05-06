899f829d795fecd1caa5e8dac1a860f6381e2233
gasiortomasz <gasior.tt@gmail.com>
2020-05-06 16:33:58 +0200

    Update masks preprocessing

--- src/dataset/preprocess.py
+++ src/dataset/preprocess.py
@@ -1,16 +1,28 @@
 import os
+import numpy as np
 
-def preprocess_np_array(folderpath, input_filepath, out_filepath):
+
+def preprocess_masks(folderpath, input_filepath, out_filepath):
     input_filepath = os.path.join(folderpath, input_filepath)
     out_filepath = os.path.join(folderpath, out_filepath)
 
-    train_images = np.load(input_filepath)
-    train_images = np.rollaxis(train_images, 3, 1)
-    train_images = train_images.astype('float32')
-    np.save(out_filepath, train_images)
+    array = np.load(input_filepath)
+    array = np.rollaxis(array, 3, 1)
+    array = array.astype('float32')
+    array[array>0.0] = 1.0
+    np.save(out_filepath, array)
+
+def preprocess_images(folderpath, input_filepath, out_filepath):
+    input_filepath = os.path.join(folderpath, input_filepath)
+    out_filepath = os.path.join(folderpath, out_filepath)
+
+    array = np.load(input_filepath)
+    array = np.rollaxis(array, 3, 1)
+    array = array.astype('float32')
+    np.save(out_filepath, array)
 
 def preprocess_data(folderpath):
-    preprocess_np_array(folderpath, 'gsn_img_uint8.npy', 'train_img_preprocessed.npy')
-    preprocess_np_array(folderpath, 'gsn_msk_uint8.npy', 'train_mask_preprocessed.npy')
-    preprocess_np_array(folderpath, 'test_gsn_image.npy', 'test_img_preprocessed.npy')
-    preprocess_np_array(folderpath, 'test_gsn_mask.npy', 'test_mask_preprocessed.npy')
+    preprocess_images(folderpath, 'gsn_img_uint8.npy', 'train_img_preprocessed.npy')
+    preprocess_masks(folderpath, 'gsn_msk_uint8.npy', 'train_mask_preprocessed.npy')
+    preprocess_images(folderpath, 'test_gsn_image.npy', 'test_img_preprocessed.npy')
+    preprocess_masks(folderpath, 'test_gsn_mask.npy', 'test_mask_preprocessed.npy')
