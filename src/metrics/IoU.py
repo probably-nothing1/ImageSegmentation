8635b4f3172731055c2909438865e00bf3310104
gasiortomasz <gasior.tt@gmail.com>
2020-05-12 22:01:34 +0200

    fix IoU

--- src/metrics/IoU.py
+++ src/metrics/IoU.py
@@ -1,25 +1,27 @@
 import torch
 
 def compute_IoU(model, dataloader, device):
+    model.eval()
     total_iou = 0
     num_images = len(dataloader.dataset)
-    model.eval()
     for i, (image, true_mask) in enumerate(dataloader):
         image, true_mask = image.to(device), true_mask.to(device)
-        predicted_mask = model(image)
-        predicted_mask[predicted_mask > 0.5] = 1.0
-        iou = compute_IoU_batch(true_mask, predicted_mask)
+        pixel_probabilities = model(image)
+        iou = compute_IoU_batch(true_mask, pixel_probabilities)
         total_iou += iou.item()
 
     model.train()
     return total_iou / num_images
 
-def compute_IoU_batch(true_mask, predicted_mask):
-    union = true_mask + predicted_mask
-    union[union > 1.0] = 1.0
+def compute_IoU_batch(true_mask, pixel_probabilities):
+    pixel_probabilities[pixel_probabilities > 0.5] = 1.0
+    pixel_probabilities[pixel_probabilities <= 0.5] = 0.0
+
+    union = true_mask + pixel_probabilities
+    union = torch.clamp(union, max=1.0)
     union = union.sum(dim=(1,2,3))
 
-    intersection = true_mask * predicted_mask
+    intersection = true_mask * pixel_probabilities
     intersection = intersection.sum(dim=(1,2,3))
 
     iou = intersection / union
