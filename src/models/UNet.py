0769a2ca290555f295734fa7188f3aade82f9eb0
gasiortomasz <gasior.tt@gmail.com>
2020-05-13 20:18:04 +0200

    Improve UNet

--- src/models/UNet.py
+++ src/models/UNet.py
@@ -5,50 +5,64 @@ import torch.nn.functional as F
 class UNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = ConvBlock(3, 32)
+        self.conv1_1 = ConvBlock(3, 32)
+        self.conv1_2 = ConvBlock(32, 32)
         self.down1 = DownSampleBlock(2)
 
-        self.conv2 = ConvBlock(32, 64)
+        self.conv2_1 = ConvBlock(32, 64)
+        self.conv2_2 = ConvBlock(64, 64)
         self.down2 = DownSampleBlock(2)
 
-        self.conv3 = ConvBlock(64, 128)
+        self.conv3_1 = ConvBlock(64, 128)
+        self.conv3_2 = ConvBlock(128, 128)
         self.down3 = DownSampleBlock(2)
 
-        self.conv4 = ConvBlock(128, 128)
+        self.conv4_1 = ConvBlock(128, 128)
+        self.conv4_2 = ConvBlock(128, 128)
 
         self.up1 = UpSampleBlock(128)
-        self.conv5 = ConvBlock(2*128, 64)
+        self.conv5_1 = ConvBlock(2*128, 64)
+        self.conv5_2 = ConvBlock(64, 64)
 
         self.up2 = UpSampleBlock(64)
-        self.conv6 = ConvBlock(2*64, 32)
+        self.conv6_1 = ConvBlock(2*64, 32)
+        self.conv6_2 = ConvBlock(32, 32)
 
         self.up3 = UpSampleBlock(32)
-        self.conv7 = ConvBlock(2*32, 1)
+        self.conv7_1 = ConvBlock(2*32, 32)
+        self.conv7_2 = ConvBlock(32, 1)
 
 
     def forward(self, x):
-        x_cache1 = self.conv1(x)
+        x = self.conv1_1(x)
+        x_cache1 = self.conv1_2(x)
         x = self.down1(x_cache1)
 
-        x_cache2 = self.conv2(x)
+        x = self.conv2_1(x)
+        x_cache2 = self.conv2_2(x)
         x = self.down2(x_cache2)
 
-        x_cache3 = self.conv3(x)
+        x = self.conv3_1(x)
+        x_cache3 = self.conv3_2(x)
         x = self.down3(x_cache3)
 
-        x = self.conv4(x)
+        x = self.conv4_1(x)
+        x = self.conv4_2(x)
 
         x = self.up1(x)
         x = torch.cat((x_cache3, x), dim=1)
-        x = self.conv5(x)
+        x = self.conv5_1(x)
+        x = self.conv5_2(x)
 
         x = self.up2(x)
         x = torch.cat((x_cache2, x), dim=1)
-        x = self.conv6(x)
+        x = self.conv6_1(x)
+        x = self.conv6_2(x)
 
         x = self.up3(x)
         x = torch.cat((x_cache1, x), dim=1)
-        x = self.conv7(x)
+        x = self.conv7_1(x)
+        x = self.conv7_2(x)
 
         return torch.sigmoid(x)
 
