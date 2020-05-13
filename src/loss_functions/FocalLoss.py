897901321fa8898b9f5cb938cfdcee401dfd1200
gasiortomasz <gasior.tt@gmail.com>
2020-05-13 16:11:37 +0200

    Update Focal Loss

--- src/loss_functions/FocalLoss.py
+++ src/loss_functions/FocalLoss.py
@@ -8,9 +8,8 @@ class FocalLoss(nn.Module):
     self.gamma = gamma
 
   def forward(self, pixel_probabilities, ground_truth):
-    gamma = 1.0
     weights = pixel_probabilities.clone().detach()
     indices = ground_truth == 1.0
-    weights[indices] = (1 - pixel_probabilities)[indices]
-    weights = weights**self.gamma
-    return F.binary_cross_entropy(pixel_probabilities, ground_truth)
+    weights[indices] = (1 - weights)[indices]
+    weights = weights ** self.gamma
+    return F.binary_cross_entropy(pixel_probabilities, ground_truth, weight=weights)
